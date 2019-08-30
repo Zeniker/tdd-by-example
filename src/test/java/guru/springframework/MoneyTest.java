a3bfc441c7250283da771f1ff71782f4ed06f747
Guilherme Lopes <gui.lls@hotmail.com>
2019-08-30 16:34:04 -0300

    Changes to Dollar class to make it immutable.

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -9,8 +9,10 @@ public class MoneyTest {
     @Test
     void testMultiplication(){
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar ten = five.times(2);
+        assertEquals(10, ten.amount);
+        Dollar fifteen = five.times(3);
+        assertEquals(15, fifteen.amount);
     }
 
 }
