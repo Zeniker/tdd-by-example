39ae910f2eeef9b7267b5100bac840eebe4608d8
Guilherme Lopes <gui.lls@hotmail.com>
2019-08-30 13:21:06 -0300

    Multiplication tested.

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,16 @@
+package guru.springframework;
+
+public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount){
+
+        this.amount = amount;
+    }
+
+    public void times(int multiplier){
+        amount *= multiplier;
+    }
+
+}
