c7a280350bba48ac33b69eebca6e18e7098cc6c8
Guilherme Lopes <gui.lls@hotmail.com>
2019-09-01 17:48:28 -0300

    Criada implementação mínima para fazer com que o teste testSimpleAddition funcione.

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -24,9 +24,19 @@ public class MoneyTest {
         assertEquals(Money.franc(5), Money.franc(5));
         assertNotEquals(Money.dollar(5), Money.franc(5));
     }
+
     @Test
     void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
+    }
+
+    @Test
+    void testSimpleAddition() {
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(10), reduced);
     }
 }
