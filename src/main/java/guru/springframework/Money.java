a5165dbdaf6e92447329c621c970cdd3c0559d06
Guilherme Lopes <gui.lls@hotmail.com>
2019-09-01 18:07:54 -0300

    Criada a classe Sum, para implementar Expression. Criada operação reduce na interface Expression. Ajustada operação reduce na classe Bank.

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -34,6 +34,11 @@ public class Money implements Expression {
     }
 
     @Override
+    public Money reduce(String to){
+        return this;
+    }
+
+    @Override
     public String toString() {
         return "Money{" +
                 "amount=" + amount +
@@ -42,6 +47,6 @@ public class Money implements Expression {
     }
 
     public Expression plus(Money  addend){
-        return new Money(amount + addend.amount, this.currency);
+        return new Sum(this, addend);
     }
 }
